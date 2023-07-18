efd21f0ae7b8a0f17242ca650697b1192228bb68
unknown <akinyigadamilare@gmail..com>
2023-07-18 01:53:33 +0100

    Adding new classifier with scaling

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel, conlist
 app = FastAPI(title="Predicting Wine Class with batching")
 
 # Open classifier in global scope
-with open("models/wine-95-fixed.pkl", "rb") as file:
+with open("course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/models/wine-95-fixed.pkl", "rb") as file:
     clf = pickle.load(file)
 
 
